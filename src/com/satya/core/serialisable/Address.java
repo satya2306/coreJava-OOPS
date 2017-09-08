e437a678ef96d2c37525bab02a85c428c6bcea65
satya2306 <satyabirapradhan@yahoo.co.in>
2017-09-08 11:22:27 +0530

    check dependant serialisation

---
+++ src/com/satya/core/serialisable/Address.java
@@ -0,0 +1,25 @@
+package com.satya.core.serialisable;
+
+import java.io.Serializable;
+
+public class Address  {
+	
+	private String state;
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Address(String city) {
+		super();
+		
+		if(city.equals("Hyderabad")){
+		this.state = "Telengana";
+		}
+	}
+	
+}
